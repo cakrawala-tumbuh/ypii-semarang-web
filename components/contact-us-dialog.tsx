6834c0f2c4796bbb129d1f5ccd188a55daf68ac5
Andhitia Rama <andhitia.r@gmail.com>
2026-03-24 03:37:43 +0700

    Membatasi pilihan unit hanya di kebon dalem

--- components/contact-us-dialog.tsx
+++ components/contact-us-dialog.tsx
@@ -107,15 +107,10 @@ export function ContactUsDialog({
                     <option value="" disabled>
                       Pilih unit
                     </option>
-                    <option value="KB-TK Cor Yesu">KB-TK Cor Yesu</option>
-                    <option value="KB-TK Kebon Dalem">KB-TK Kebon Dalem</option>
-                    <option value="KB-TK Kebon Dalem 2">KB-TK Kebon Dalem 2</option>
-                    <option value="SD Cahaya Nur">SD Cahaya Nur</option>
                     <option value="SD Kebon Dalem">SD Kebon Dalem</option>
                     <option value="SD Kebon Dalem 2">SD Kebon Dalem 2</option>
-                    <option value="SD Maria Bintang Laut">SD Maria Bintang Laut</option>
-                    <option value="SD Pangudi Utami">SD Pangudi Utami</option>
                     <option value="SMA Kebon Dalem">SMA Kebon Dalem</option>
+                    <option value="SMP Kebon Dalem">SMP Kebon Dalem</option>
                   </select>
                 </div>
 
